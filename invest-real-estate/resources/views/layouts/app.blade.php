c61067c51b82e89402b3a844f8c765754cc4e714
Ksenya Kiseleva <vvorobushekk7@gmail.com>
2025-04-30 16:01:48 +0500

    UI

--- invest-real-estate/resources/views/layouts/app.blade.php
+++ invest-real-estate/resources/views/layouts/app.blade.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-{{--    <link rel="icon" type="image/svg" href="/svg/favicon-MP.svg">--}}
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="32x32" type="image/x-icon">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="192x192" type="image/png">
+    <link rel="apple-touch-icon" href="{{ asset('favicon.ico') }}">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="/css/libs.min.css" />
     <link rel="stylesheet" href="/css/jquery.fancybox.min.css" />
@@ -12,7 +15,7 @@
     <script src="/js/libs.min.js"></script>
     <script src="/js/jquery.fancybox.min.js"></script>
     @vite(['resources/scss/main.scss', 'resources/js/app.js'])
-    <title>MASTER PING - простые покупки сложных вещей</title>
+    <title>Новый инструмент инвестирования в зарубежную недвижимость</title>
 </head>
 <body>
 {{--@include('partials/site/header')--}}
