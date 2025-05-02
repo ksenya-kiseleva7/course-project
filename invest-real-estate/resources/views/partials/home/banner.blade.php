ecf2a2126a8cf943e1d855d28aa0c155a99ae1ef
Ksenya Kiseleva <vvorobushekk7@gmail.com>
2025-05-02 13:48:46 +0500

    refactor: баннер

---
+++ invest-real-estate/resources/views/partials/home/banner.blade.php
@@ -0,0 +1,45 @@
+<div class="banner-section">
+    <img class="banner-section__img" src="images/banner.png" alt="">
+    <h2 class="h2 banner-section__text">
+        Новый инструмент<br>инвестирования в&nbsp;зарубежную<br>недвижимость
+    </h2>
+    <div class="container">
+        <div class="cards-row">
+            <x-card.advantages
+                title="Продумано"
+                description="Инвестиции&nbsp;распределены
+в&nbsp;4-х странах: Индонезия, Тайланд, Грузия, Азербайджан"
+                slide="false"
+                class="card-anim"
+            />
+            <x-card.advantages
+                title="Продумано"
+                description="Инвестиции&nbsp;распределены
+в&nbsp;4-х странах: Индонезия, Тайланд, Грузия, Азербайджан"
+                slide="false"
+                class="card-anim"
+            />
+            <x-card.advantages
+                title="Продумано"
+                description="Инвестиции&nbsp;распределены
+в&nbsp;4-х странах: Индонезия, Тайланд, Грузия, Азербайджан"
+                slide="false"
+                class="card-anim"
+            />
+            <x-card.advantages
+                title="Продумано"
+                description="Инвестиции&nbsp;распределены
+в&nbsp;4-х странах: Индонезия, Тайланд, Грузия, Азербайджан"
+                slide="false"
+                class="card-anim"
+            />
+            <x-card.advantages
+                title="Продумано"
+                description="Инвестиции&nbsp;распределены
+в&nbsp;4-х странах: Индонезия, Тайланд, Грузия, Азербайджан"
+                slide="false"
+                class="card-anim"
+            />
+        </div>
+    </div>
+</div>
