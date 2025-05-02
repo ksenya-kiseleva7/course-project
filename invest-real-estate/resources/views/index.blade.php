592d5d4688b4d9d2e47076bc04a486f8deebb14e
Ksenya Kiseleva <vvorobushekk7@gmail.com>
2025-05-02 13:49:19 +0500

    index

---
+++ invest-real-estate/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <main>
+        @include('partials/home/banner')
+        @include('partials/home/products')
+{{--        @include('partials/home/about')--}}
+{{--        @include('partials/home/project')--}}
+{{--        @include('partials/home/servise')--}}
+{{--        @include('partials/home/reviews')--}}
+{{--        @include('partials/home/news')--}}
+{{--        @include('partials/site/contact')--}}
+    </main>
+@endsection
